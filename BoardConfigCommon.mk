60abe10eb20f1d6224b7665836ce91ec2bdc79d2
blkredstarV <blkredstar5@gmail.com>
2015-03-05 11:27:32 -0600

    Revert "change kernel path"
    This reverts commit e320c564fbeac18f03861120c83a611e29b3e87e.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,7 +37,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := m7_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/m7
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # Audio
 BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
