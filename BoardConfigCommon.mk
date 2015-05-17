80804e2f7edd3f54aac234ee1af86759578f7306
blkredstarV <blkredstar5@gmail.com>
2015-05-16 21:28:14 -0500

    back to 4.9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,8 +36,11 @@ BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
-TARGET_KERNEL_CONFIG := broken_m7_defconfig
+TARGET_KERNEL_CONFIG := cyanogen_m7_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm8960
+
+# Custom Kernel Toolchains 
+KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-linaro/bin/"
 
 # Audio
 BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
